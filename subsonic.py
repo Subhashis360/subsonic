07abe442e1accca53c367b1f2db9ae10c797c06d
Subhashis <113136062+Subhashis360@users.noreply.github.com>
2024-01-17 21:44:13 +0530

    Create subsonic.py

---
+++ subsonic.py
@@ -0,0 +1,123 @@
+from concurrent.futures import ThreadPoolExecutor
+import re
+import httpx
+import datetime as dt
+from fake_useragent import UserAgent
+import requests
+
+G = '\033[1;32m'
+R = '\033[1;31m'
+
+print((G+"""
+  _____________ _____________  ___ ___    _____    _________ ___ ___ .___  _________
+ /   _____/    |   \______   \/   |   \  /  _  \  /   _____//   |   \|   |/   _____/
+ \_____  \|    |   /|    |  _/    ~    \/  /_\  \ \_____  \/    ~    \   |\_____  \ 
+ /        \    |  / |    |   \    Y    /    |    \/        \    Y    /   |/        /
+/_______  /______/  |______  /\___|_  /\____|__  /_______  /\___|_  /|___/_______  /
+        \/                 \/       \/         \/        \/       \/             \/ 
+
+"""))
+
+print(f"{R}                                       SUBSONIC                         ")
+print(f"{G}                               Fastest Subdomain Finder                         ")
+
+print("\n")
+
+domain = input(G+"[=>] Enter the Domain Name : ").lower().replace("http://", "").replace("https://", "").replace("www.", "").rstrip("/")
+filename = f"{domain}.txt".strip()
+output_file = f"{filename}"
+
+yesterday = dt.date.today() - dt.timedelta(days=1)
+
+subdomains_set = set()
+unique_urls = set()
+
+def get_subdomains(url, pattern, domain):
+    try:
+        with httpx.Client() as request:
+            user_agent = UserAgent().random
+            headers = {'User-Agent': user_agent}
+            response = request.get(url, timeout=10, headers=headers)
+            if response.status_code == 200:
+                content = response.text
+                subdomains = pattern.findall(content)
+                for subdomain in subdomains:
+                    if subdomain.endswith(f".{domain}"):
+                        print(G+"[*] Found => "+subdomain)
+                        subdomains_set.add(subdomain)
+    except Exception as e:
+        pass
+
+def probe_url(url, unique_urls):
+    try:
+        user_agent = UserAgent().random
+        headers = {'User-Agent': user_agent}
+
+        http_url = "http://" + url
+        http_response = requests.get(http_url, timeout=5, headers=headers)
+        if http_response.status_code == 200:
+            print(G+f"[*] Active Found => {http_url}")
+            unique_urls.add(http_url)
+            with open(output_file, "a") as file:
+                file.write(f"{http_url}\n")
+
+        https_url = "https://" + url
+        https_response = requests.get(https_url, timeout=5, headers=headers)
+        if https_response.status_code == 200:
+            print(G+f"[*] Active Found => {https_url}")
+            unique_urls.add(https_url)
+            with open(output_file, "a") as file:
+                file.write(f"{https_url}\n")
+
+    except requests.RequestException:
+        pass
+
+def process_source(source):
+    source_url, source_pattern = source
+    get_subdomains(source_url.format(domain=domain), source_pattern, domain)
+
+sources = [
+    ("https://www.abuseipdb.com/whois/{domain}", re.compile(r'<li>\w.*</li>')),
+    ("https://otx.alienvault.com/api/v1/indicators/hostname/{domain}/passive_dns", re.compile(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")),
+    ("https://api.subdomain.center/?domain={domain}", re.compile(r"")),
+    ("https://columbus.elmasy.com/api/lookup/{domain}?days=-1", re.compile(r"")),
+    ("https://jonlu.ca/anubis/subdomains/{domain}", re.compile(r"")),
+    ("https://dnsrepo.noc.org/?domain={domain}", re.compile(rf"\b[a-zA-Z0-9]+\b\.{domain}\b")),
+    ("https://api.hackertarget.com/hostsearch/?q={domain}", re.compile(r"")),
+    ("https://rapiddns.io/subdomain/{domain}?full=1", re.compile(rf"\b[a-zA-Z0-9]+\b\.{domain}\b")),
+    ("https://urlscan.io/api/v1/search/?q=page.domain:{domain}&size=10000", re.compile(r"")),
+    ("http://web.archive.org/cdx/search/cdx?url=*.{domain}/*&output=txt&fl=original&collapse=urlkey", re.compile(r"https://([\w-]+\.{domain})")),
+    ("https://seckrd.com/subdomain-finder.php", re.compile(rf"https://([\w-]+\.{domain})")),
+    ("https://crt.sh/?q=%25.{domain}", re.compile(rf"\b[a-zA-Z0-9.*-]+\.{re.escape(domain)}\b")),
+    ("https://shrewdeye.app/domains/{domain}.txt", re.compile(rf"\b[a-zA-Z0-9]+\b\.{domain}\b")),
+    ("https://subdomainfinder.c99.nl/scans/{yesterday}/{domain}", re.compile(rf"https?://([\w-]+\.{domain})")),
+]
+
+with ThreadPoolExecutor(max_workers=300) as executor:
+    executor.map(process_source, sources)
+
+
+with open(f"{domain}.txt", "w") as file:
+    for subdomain in subdomains_set:
+        file.write(subdomain + "\n")
+print(G+f"[=>] Subdomains for {domain} have been saved in [[{domain}.txt]]")
+
+probe_input = input(R+f"[=>] Do you want to Fetch Working Subdomains From this {domain} ? (yes/no): ").lower().strip()
+
+if probe_input.startswith("y"):
+    with open(filename, "r") as file:
+        urls = file.read().splitlines()
+
+    with ThreadPoolExecutor(max_workers=20) as executor:
+        executor.map(lambda url: probe_url(url, unique_urls), urls)
+        
+    if unique_urls:
+        with open(output_file, "w") as file:
+            file.write("\n".join(unique_urls))
+        print(R+f"[=>] Active Results have been saved in [[{output_file}]]")
+        print(G+"[=>] Thanks For using Subsonic")
+    else:
+        print(R+f"[=>] No active Subdomain Found")
+        print(G+"[=>] Thanks For using Subsonic")
+else:
+    print(G+"[=>] Thanks For using Subsonic")
